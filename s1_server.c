5284f723bb2303943a4d7e9c0e5942050aec3027
priyan_rajamohan <rajamohanpriyan@gmail.com>
2021-04-06 12:33:45 +0800

    managed to capture the response as wanted

--- s1_server.c
+++ s1_server.c
@@ -8,8 +8,16 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <stdarg.h>
 
 #define MAX_DATA 4096
+#define SERVER_PORT 1200
 
 void main()
 {
@@ -17,7 +25,7 @@ void main()
     int listenfd, connfd;          /* socket descriptor */
     struct sockaddr_in s1, client; /* variable names for the socket addr data structure */
     int cli_len = sizeof(client);
-    int total, sent, bytes, received;
+    int total, sent, received;
     char buf[MAX_DATA]; /* data sent by client stored in buf */
     char *reply =
         "HTTP/1.1 200 OK\r\n"
@@ -35,7 +43,7 @@ void main()
     }
 
     s1.sin_family = AF_INET;
-    s1.sin_port = htons(1200); /* using port 1200 for s1 */
+    s1.sin_port = htons(SERVER_PORT); /* using port 1200 for s1 */
     s1.sin_addr.s_addr = htonl(INADDR_ANY);
     bzero(&s1.sin_zero, 0);
 
@@ -56,26 +64,12 @@ void main()
     /* accepting with implementation of concurrent server using fork() */
     while (1)
     {
-
         connfd = accept(listenfd, (struct sockaddr *)&client, &cli_len);
         if (connfd < 0)
         {
             perror("accept");
             exit(1);
         }
-
-        /* sending the HTML file*/
-        total = strlen(reply);
-        sent = 0;
-        do
-        {
-            int bytes = write(connfd, reply + sent, total - sent);
-            if (bytes < 0)
-                perror("ERROR writing message to socket");
-            if (bytes == 0)
-                break;
-            sent += bytes;
-        } while (sent < total);
 
         printf("New Client connected from port number %d and IP address  %s\n", ntohs(client.sin_port), inet_ntoa(client.sin_addr));
 
@@ -86,35 +80,32 @@ void main()
             /* processing */
 
             /* receiving response */
-
             memset(buf, 0, sizeof(buf));
-            total = sizeof(buf) - 1;
             received = 0;
-            do
-            {
-                bytes = read(connfd, buf + received, total - received);
-                if (bytes < 0)
-                    perror("ERROR reading response from socket");
-                if (bytes == 0)
-                    break;
-                received += bytes;
-            } while (received < total);
 
-            if (received == total)
-                perror("ERROR storing complete response from socket");
+            received = recv(connfd, buf, MAX_DATA - 1, 0);
 
-            buf[bytes] = '\0';
+            buf[received] = '\0';
             printf("Printing BUF in child: %s\n\n\n", buf);
-
-            // char *token = NULL;
-            // token = strtok(buf, "\n");
-            // while (token)
-            // {
-            //     printf("Current token: %s.\n", token);
-            //     token = strtok(NULL, "\n");
-            // }
-
             printf("received %d in child\n\n", received);
+
+            if (received <= 0)
+            {
+                perror("Didnt read any from client");
+                exit(1);
+            }
+
+            /* sending the HTML file*/
+            sent = 0;
+            sent = send(connfd, reply, strlen(reply),0);
+
+            printf("sent : %s", reply);
+
+            if (sent <= 0)
+            {
+                perror("Didnt send any from server");
+                exit(1);
+            }
 
             printf("Closing Child\n\n");
             close(connfd);
@@ -127,3 +118,34 @@ void main()
     printf("Closing Parent\n");
     exit(0);
 }
+
+// /// Write the request
+// if (write(sockfd, sendline, strlen(sendline)) >= 0)
+// {
+// }
+
+//        total = sizeof(buf) - 1;
+//             received = 0;
+//             do
+//             {
+//                 bytes = read(connfd, buf + received, total - received);
+//                 if (bytes < 0)
+//                     perror("ERROR reading response from socket");
+//                 if (bytes == 0)
+//                     break;
+//                 received += bytes;
+//             } while (received < total);
+
+//             if (received == total)
+//                 perror("ERROR storing complete response from socket");
+//   total = strlen(reply);
+//             sent = 0;
+//             do
+//             {
+//                 int bytes = write(connfd, reply + sent, total - sent);
+//                 if (bytes < 0)
+//                     perror("ERROR writing message to socket");
+//                 if (bytes == 0)
+//                     break;
+//                 sent += bytes;
+//             } while (sent < total);
